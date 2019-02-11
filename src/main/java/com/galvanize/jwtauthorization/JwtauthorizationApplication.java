4759cf0bab2859e6510d3865a4701f7c32d0716e
jeff-lent <jefflent01@gmail.com>
2019-02-11 04:43:36 -0500

    initial commit

---
+++ src/main/java/com/galvanize/jwtauthorization/JwtauthorizationApplication.java
@@ -0,0 +1,14 @@
+package com.galvanize.jwtauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtauthorizationApplication.class, args);
+    }
+
+}
+
